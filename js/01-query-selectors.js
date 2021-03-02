784d464e77b88183912b9432842612aefad77eab
Ruslan4444 <ruslankv4@gmail.com>
2021-03-02 21:28:06 +0100

    Пройдено теорію по 1 та 2 розділі

---
+++ js/01-query-selectors.js
@@ -0,0 +1,26 @@
+const magicBtn = document.querySelector('.js-magic-btn');
+
+// console.dir(document);
+
+// const navEl = document.querySelector('ul');
+// console.log(navEl);
+
+// const navItemEl = document.querySelector('li');
+// console.log(navItemEl);
+
+// Can find first one with this class
+// const navItemRef = document.querySelector('.site-nav__item');  
+// console.log(navItemRef);
+
+// If nothing found, we get in console = null
+// const navItemRef = document.querySelector('.site-nav__item111');  
+// console.log(navItemRef);
+
+// If I want to find list of links from ul
+// const navLinksEL = document.querySelectorAll('.site-nav__link');
+// console.log(navLinksEL);
+
+// If to write in querySelectorAll wrong css class we get empty NodeList [] 
+// const navLinksEL = document.querySelectorAll('.site-nav__link5555');
+// console.log(navLinksEL);
+
---
+++ js/02-properties.js
@@ -0,0 +1,34 @@
+const magicBtn = document.querySelector('.js-magic-btn');
+
+const imageEl = document.querySelector('.hero__image');
+// console.log('image', imageEl);
+// console.log(imageEl.src);
+
+// If I want to change property of imageEl src
+// imageEl.src = 'https://images.pexels.com/photos/57416/cat-sweet-kitty-animals-57416.jpeg?auto = compress & cs=tinysrgb & dpr=2 & w=480'
+
+// magicBtn.addEventListener('click', () => {
+//     const imageEl = document.querySelector('.hero__image');
+//     console.log('image', imageEl);
+//     console.log(imageEl.src);
+//     imageEl.alt = 'It is cat';
+
+// });
+
+// const heroTitleEl = document.querySelector('.hero__title');
+// console.log('heroTitleEl', heroTitleEl);
+// console.log(heroTitleEl.textContent);
+
+// If I want to change textContent
+// heroTitleEl.textContent = 'I am sweet cake';
+
+// console.log(imageEl.getAttribute('src'));
+// console.log(imageEl);
+
+// hasAttribute can return in console true or false
+// console.log(imageEl.hasAttribute('src'));
+
+const actions = document.querySelectorAll('.js-actions button');
+// console.log(actions);
+console.log(actions[0].dataset);
+console.log(actions[0].dataset.action);
